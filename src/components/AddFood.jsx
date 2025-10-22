7dd0c510567edd028c4f8be658e43615af443ce7
msaheers <mishalsaheersha10@gmail.com>
2025-10-22 17:23:42 +0100

    feat: updated AddAnimal.jsx and AddFood.jsx components

---
+++ src/components/AddFood.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios"; 
+
+const AddFood = () => {
+    const [formData, setFormData] = useState({
+    name: "" , 
+    totalKg: "",
+    photoFoodUrl: "",
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post("http://localhost:5000/api/foods", formData);
+            alert("✅ Comida adicionada com sucesso!");
+            setFormData({ name: "", totalKg: "", photoFoodUrl: ""});
+        } catch (err) {
+            console.error(err);
+            alert("❌ Erro ao adicionar comida");
+        }
+    };
+
+    return (
+        <div className="add-food">
+            <h2>Adicionar Comida</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Nome da Comida"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type="number"
+                    name="totalKg"
+                    placeholder="Quantidade Total (kg)"
+                    value={formData.totalKg}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type="text"
+                    name="photoFoodUrl"
+                    placeholder="URL da Foto"
+                    value={formData.photoFoodUrl}
+                    onChange={handleChange}
+                    required
+                />
+                <button type="submit">Adicionar</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddFood;
